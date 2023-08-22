db1541217696e1a0814a87c6900384b86d8a7b31
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-08-22 20:23:28 +0300

    Create VKBigChatStickers.user.js

---
+++ VKBigChatStickers/VKBigChatStickers.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         VK Big Chat Stickers
+// @namespace    https://github.com/Maks1mS/userscripts
+// @version      0.1
+// @description  Increases size of chat stickers
+// @author       Maxim Slipenko
+// @match        https://vk.com/im*
+// @icon         https://vk.com/favicon.ico
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const style = `
+    img.StickerUGC {
+        width: 256px;
+        height: 256px;
+    }
+    `;
+
+    GM_addStyle(style)
+})();
