290120ba0943cb89d300fc4cd4d69ab7013a524e
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-08-31 18:24:27 +0300

    [VKBigChatStickers] Increase size inside sticker menu

--- VKBigChatStickers/VKBigChatStickers.user.js
+++ VKBigChatStickers/VKBigChatStickers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VK Big Chat Stickers
 // @namespace    https://github.com/Maks1mS/userscripts
-// @version      0.1
+// @version      0.2
 // @description  Increases size of chat stickers
 // @author       Maxim Slipenko
 // @match        https://vk.com/*
@@ -16,6 +16,17 @@
     img.StickerUGC {
         width: 256px;
         height: 256px;
+    }
+
+    .StickersEmojiKeyboard-module__in--GCR5_:has(.StickersKeyboardGroupItem-module__in--CKjkC.StickersKeyboardGroupItem-module__inActive--iqstp > div[aria-label="Стикеры чата"]) .StickersKeyboard-module__keyboard--HhSGu.View-module__panel--WNKKi.View-module__panelSwitched--hg3jP {
+       --stickers-columns: 2 !important;
+       --stickers-size: 172px !important;
+    }
+    .StickersEmojiKeyboard-module__in--GCR5_:has(.StickersKeyboardGroupItem-module__in--CKjkC.StickersKeyboardGroupItem-module__inActive--iqstp > div[aria-label="Стикеры чата"]) .Sticker-module__sticker--CtZU6.Sticker-module__tappable--M4lob.StickersKeyboardRow-module__sticker--m7W0R {
+       --sticker-size: 172px !important;
+       --sticker-scale: 2 !important;
+       width: 172px !important;
+       height: 172px !important;
     }
     `;
 
