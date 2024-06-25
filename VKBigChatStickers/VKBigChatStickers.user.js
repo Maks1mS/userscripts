c262b5173d8ab4b23aec49cdbdb42a28429a12ab
KaMZeSs <vladd267@gmail.com>
2024-06-25 10:42:34 +0300

    Adjusted the position of the sticker icon

--- VKBigChatStickers/VKBigChatStickers.user.js
+++ VKBigChatStickers/VKBigChatStickers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VK Big Chat Stickers
 // @namespace    https://github.com/Maks1mS/userscripts
-// @version      0.2
+// @version      0.3
 // @description  Increases size of chat stickers
 // @author       Maxim Slipenko
 // @match        https://vk.com/*
@@ -9,24 +9,30 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     const style = `
-    img.StickerUGC {
+    .sticker_img_wrapper {
+        display: inline-block; /* Обеспечивает подгонку размеров под содержимое */
         width: 256px;
         height: 256px;
     }
+    img.StickerUGC {
+        display: block;
+        height: 100%;
+        width: 100%;
+    }
 
     .StickersEmojiKeyboard-module__in--GCR5_:has(.StickersKeyboardGroupItem-module__in--CKjkC.StickersKeyboardGroupItem-module__inActive--iqstp > div[aria-label="Стикеры чата"]) .StickersKeyboard-module__keyboard--HhSGu.View-module__panel--WNKKi.View-module__panelSwitched--hg3jP {
-       --stickers-columns: 2 !important;
-       --stickers-size: 172px !important;
+        --stickers-columns: 2 !important;
+        --stickers-size: 172px !important;
     }
     .StickersEmojiKeyboard-module__in--GCR5_:has(.StickersKeyboardGroupItem-module__in--CKjkC.StickersKeyboardGroupItem-module__inActive--iqstp > div[aria-label="Стикеры чата"]) .Sticker-module__sticker--CtZU6.Sticker-module__tappable--M4lob.StickersKeyboardRow-module__sticker--m7W0R {
-       --sticker-size: 172px !important;
-       --sticker-scale: 2 !important;
-       width: 172px !important;
-       height: 172px !important;
+        --sticker-size: 172px !important;
+        --sticker-scale: 2 !important;
+        width: 172px !important;
+        height: 172px !important;
     }
     `;
 
